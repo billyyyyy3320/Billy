f9db05e55a686798cc33a925e40ff1cf8ac7bbda
Billyyyyy3320 <newsbielt703@gmail.com>
2020-01-14 01:25:45 +0800

    feat: imporve home page layout in mobile

--- blog/.vuepress/components/GetStarted.vue
+++ blog/.vuepress/components/GetStarted.vue
@@ -4,17 +4,28 @@
   </div>
 </template>
 
-<style scoped>
-.start {
-  margin: 50px 0;
-}
-.start > a {
-  text-decoration: none;
-  font-size: 1.2rem;
-  border: 1px solid;
-  padding: 0.8rem 1.6rem;
-  border-radius: 4px;
-  transition: 0.1s ease;
-  box-sizing: border-box;
-}
+<style lang="stylus" scoped>
+.start
+  margin 50px 0
+
+  > a
+    text-decoration none
+    font-size 1.2rem
+    border 1px solid
+    padding 0.8rem 1.6rem
+    border-radius 4px
+    transition 0.1s ease
+    box-sizing border-box
+
+  @media (max-width: $MQMobile)
+    display flex
+    justify-content center
+
+p
+  font-size 20px
+
+@media (max-width: $MQMobile)
+  .beginning
+    margin-top 0 !important
+    text-align center
 </style>
--- blog/Readme.md
+++ blog/Readme.md
@@ -2,7 +2,7 @@
 title: About
 ---
 
-# Hi, I'm Billy.
+<h1 class="beginning"> Hi, I'm Billy </h1>
 
 I was born in 1997. I work as a frontend engineer. Developing is one of my habits. I make open source contributions and became a part of [VuePress](https://github.com/vuejs/vuepress) core team. I'm currently open to job opportunities.
 
@@ -10,8 +10,12 @@ I'm going to share some experiences here. It might help myself organize my thoug
 
 <GetStarted/>
 
-<style scoped>
-p{
-    font-size:20px;
-}
+<style lang="stylus" scoped>
+p
+  font-size 20px
+
+@media (max-width: $MQMobile)
+  .beginning
+    margin-top 0 !important
+    text-align center
 </style>
